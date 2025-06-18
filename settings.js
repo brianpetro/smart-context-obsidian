c584857841ff5f9f6988d6fe2e2daa042965961d
Brian Joseph Petro <brian@smartconnections.app>
2025-06-18 08:09:56 -0400

    Add getting started guide button to settings tab

--- settings.js
+++ settings.js
@@ -1,11 +1,5 @@
-/******************************************************
- * main.js
- * @fileoverview
- * Obsidian plugin entry point for Smart Context.
- ******************************************************/
-import {
-  PluginSettingTab
-} from 'obsidian';
+import { StoryModal } from 'obsidian-smart-env/modals/story.js';
+import { PluginSettingTab } from 'obsidian';
 
 /**
  * A simple plugin settings tab that delegates config to the env.smart_view system.
@@ -20,6 +14,20 @@ export class SmartContextSettingTab extends PluginSettingTab {
   display() {
     const { containerEl } = this;
     containerEl.empty();
+    const gs_container = containerEl.createEl('div', {
+      cls: 'smart-context-getting-started-container',
+    });
+    gs_container.style.marginBottom = '1em';
+    gs_container.createEl('button', {
+      cls: 'sc-getting-started-button',
+      text: 'Getting started guide',
+    });
+    gs_container.querySelector('.sc-getting-started-button').addEventListener('click', () => {
+      StoryModal.open(this.plugin, {
+        title: 'Getting Started With Smart Context',
+        url: 'https://smartconnections.app/story/smart-context-getting-started/?utm_source=context-settings',
+      });
+    });
 
     Object.entries(this.plugin.env.smart_contexts.settings_config).forEach(
       ([setting, config]) => {
