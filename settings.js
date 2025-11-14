8fede578046910f50330bec6a7212224bdf1b29c
Brian Joseph Petro <brian@smartconnections.app>
2025-11-14 16:20:13 -0500

    Refactor SmartContextSettingTab to extend SmartPluginSettingsTab and streamline render_plugin_settings method

--- settings.js
+++ settings.js
@@ -1,47 +1,48 @@
 import { StoryModal } from 'obsidian-smart-env/modals/story.js';
-import { PluginSettingTab } from 'obsidian';
+import { SmartPluginSettingsTab } from 'obsidian-smart-env';
 
 /**
  * A simple plugin settings tab that delegates config to the env.smart_view system.
  */
-export class SmartContextSettingTab extends PluginSettingTab {
+export class SmartContextSettingTab extends SmartPluginSettingsTab {
   constructor(app, plugin) {
     super(app, plugin);
     this.plugin = plugin;
-    this.env = plugin.env;
   }
 
-  display() {
-    const { containerEl } = this;
-    containerEl.empty();
-    const gs_container = containerEl.createEl('div', {
+  async render_plugin_settings(container) {
+    if (!container) return;
+    container.empty?.();
+    const getting_started_container = container.createDiv({
       cls: 'smart-context-getting-started-container',
     });
-    gs_container.style.marginBottom = '1em';
-    gs_container.createEl('button', {
+    getting_started_container.style.marginBottom = '1em';
+    const launch_button = getting_started_container.createEl('button', {
       cls: 'sc-getting-started-button',
       text: 'Getting started guide',
     });
-    gs_container.querySelector('.sc-getting-started-button').addEventListener('click', () => {
+    launch_button.addEventListener('click', () => {
       StoryModal.open(this.plugin, {
         title: 'Getting Started With Smart Context',
         url: 'https://smartconnections.app/story/smart-context-getting-started/?utm_source=context-settings',
       });
     });
 
-    Object.entries(this.plugin.env.smart_contexts.settings_config).forEach(
-      ([setting, config]) => {
-        const setting_html = this.env.smart_view.render_setting_html({
-          setting,
-          ...config,
-        });
-        const frag = this.env.smart_view.create_doc_fragment(setting_html);
-        containerEl.appendChild(frag);
-      }
-    );
+    const smart_view = this.env?.smart_view;
+    const smart_contexts = this.plugin?.env?.smart_contexts;
+    if (!smart_view || !smart_contexts) return;
 
-    this.env.smart_view.render_setting_components(containerEl, {
-      scope: this.plugin.env.smart_contexts,
+    Object.entries(smart_contexts.settings_config || {}).forEach(([setting, config]) => {
+      const setting_html = smart_view.render_setting_html({
+        setting,
+        ...config,
+      });
+      const fragment = smart_view.create_doc_fragment(setting_html);
+      if (fragment) container.appendChild(fragment);
+    });
+
+    await smart_view.render_setting_components(container, {
+      scope: smart_contexts,
     });
   }
 }
